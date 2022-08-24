28630ab41b0efe206474c7c67f9c9ff8c5b8f339
JoyceFS <joycesoave6070@gmail.com>
2022-08-24 15:50:01 -0300

    desafio

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,7 @@
-Joyce
-joycesoave6070@gmail.com
-estudante
-17
-Nova York
+console.log("Joyce")
+console.log("joycesoave6070@gmail.com")
+console.log("estudante")
+console.log("17")
+console.log("Nova York")
+
+Olá, Joyce! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para Nova YOrk. Confirma pra gente alguns dados? A sua idade é 17, você é estudante e o seu email é joycesoave6070@gmail.com? Ficamos no aguardo. Muito obrigado e boa viagem! :D
