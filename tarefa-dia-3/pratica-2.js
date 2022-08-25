0505583390a23757fc0c0435d6b42c3258c93450
JoyceFS <joycesoave6070@gmail.com>
2022-08-25 20:58:46 -0300

    tres

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,8 @@
+let n 
+let z
+while (n)
+
+n*9 = z  
+console.log (z)
+
+
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,10 @@
+let n 
+while (n)
+
+console.log (1)
+console.log (3)
+console.log (5)
+console.log (7)
+console.log (9)
+console.log (11)
+console.log (13)
