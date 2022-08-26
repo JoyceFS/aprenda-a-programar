9f9784f4ce48ce96bd82bac6c5034b7633407dd6
JoyceFS <joycesoave6070@gmail.com>
2022-08-25 21:18:16 -0300

    dois

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -1,8 +1,13 @@
 let n 
-let z
 while (n)
 
-n*9 = z  
-console.log (z)
-
-
+console.log (9 x 1 = 9)
+console.log (9 x 2 = 18)
+console.log( 9 x 3 = 27)
+    console.log (9 x 4 = 36)
+    console.log (9 x 5 = 45)
+    console.log (9 x 6 = 54)
+    console.log (9 x 7 = 63)
+    console.log (9 x 8 = 72)
+    console.log (9 x 9 = 81)
+    console.log (9 x 10 = 90)
